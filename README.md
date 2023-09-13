7fbf56b9e5afd20725bec4cd0d144e6ea9c99417
sebelumcahahaya <anandadimmas1204@gmail.com>
2023-09-13 11:09:20 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<img src="/1.jpg" width="100%" >
+
+instalasi api
+
+-composer update
+
+-php artisan key:generate
+
+-php artisan migrate:fresh --seed
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        User::create([
+            'name'      => 'Fika Ridaul Maulayya',
+            'email'     => 'admin@gmail.com',
+            'password'  => bcrypt('password')
+        ]);
+        User::create([
+            'name'      => 'dimmas',
+            'email'     => 'dimmas@gmail.com',
+            'password'  => bcrypt('password')
+        ]);
+    }
+}
